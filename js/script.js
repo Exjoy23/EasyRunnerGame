12d48d6088b1893a4b032ec799e6bc7d6e1f529e
Alexey Lazarenko <70312230+Exjoy23@users.noreply.github.com>
2021-01-12 12:59:09 +0300

    add platforms

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 300 }
+      gravity: { y: 500 }
     }
   },
   scene: {
@@ -26,30 +26,23 @@ const game = new Phaser.Game(config);
 
 let player;
 let jump;
-let platforms;
-let platforms1;
-let platforms2;
+let score = 0;
+
+const platforms = [];
+
+let platformPositionX = 600;
+let platformPositionY = 350;
 
 function preload() {
   this.load.image('background', 'img/background.png');
-  this.load.image('meteorite', 'img/meteorite.png');
-  this.load.spritesheet('hero', 'img/hero.png', { frameWidth: 104, frameHeight: 116 });
+  this.load.spritesheet('platform', 'img/platform.png', { frameWidth: 180, frameHeight: 33 });
+  this.load.spritesheet('hero', 'img/hero.png', { frameWidth: 78, frameHeight: 87 });
 }
 
 function create() {
-  // this.add.image(400, 225, 'background');
-
   this.bg = this.add.tileSprite(0, 0, 800, 450, 'background').setOrigin(0, 0);
 
   player = this.physics.add.sprite(400, 100, 'hero');
-
-  // player.setCollideWorldBounds(true);
-
-  platforms = this.physics.add.sprite(600, 350, 'meteorite');
-  platforms1 = this.physics.add.sprite(1000, 350, 'meteorite');
-  platforms2 = this.physics.add.sprite(1400, 350, 'meteorite');
-
-  // platforms.setCollideWorldBounds(true);
 
   this.anims.create({
     key: 'walk',
@@ -65,16 +58,25 @@ function create() {
     repeat: -1
   });
 
+  this.anims.create({
+    key: 'move',
+    frames: this.anims.generateFrameNumbers('platform', { start: 0, end: 3 }),
+    frameRate: 16,
+    repeat: -1
+  });
+
   player.play('walk');
 
   this.physics.add.collider(player, platforms);
-  this.physics.add.collider(player, platforms1);
-  this.physics.add.collider(player, platforms2);
 }
 
 function update() {
+  if (player.y > 500) {
+    platforms.length = 0;
+  }
+
   if (jump && (player.body.blocked.down || player.body.touching.down)) {
-    player.setVelocityY(-300);
+    player.setVelocityY(-360);
 
     player.anims.play('up', true);
   } else if (player.body.blocked.down || player.body.touching.down) {
@@ -82,12 +84,27 @@ function update() {
   }
 
   this.bg.tilePositionX += 1;
-  platforms.setVelocityX(-180);
-  platforms.setVelocityY(0);
-  platforms1.setVelocityX(-180);
-  platforms1.setVelocityY(0);
-  platforms2.setVelocityX(-180);
-  platforms2.setVelocityY(0);
+
+  if (platforms.length < 3) {
+    // if (Math.random() < 0.3) {
+    //   platforms.push(this.physics.add.sprite(platformPositionX, platformPositionY, 'platform').setScale(1.3));
+    // } else {
+    //   platforms.push(this.physics.add.sprite(platformPositionX, platformPositionY, 'platform'));
+    // }
+    platforms.push(this.physics.add.sprite(platformPositionX, platformPositionY, 'platform'));
+  }
+
+  platforms.forEach(item => {
+    if (item.x < 250) {
+      platforms.shift();
+      score++;
+    }
+    item.setVelocityY(-5);
+    item.setVelocityX(-250);
+    item.anims.play('move', true);
+    platformPositionX = item.x + 500 - Math.floor(Math.random() * 100);
+    platformPositionY = 350 - Math.floor(Math.random() * 100);
+  });
 }
 
 document.addEventListener('touchstart', () => {
